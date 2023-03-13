53c8819f85db2ccc7c4dd6f57db5ba8ea5ac1ee9
Matheus Vital <matheusvital48@gmail.com>
2023-03-13 15:47:55 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,12 @@ namespace SalesWebMvc.Controllers
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Aluno"] = "Matheus Vital";
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
